13c6e06c9f66f69e77a5145afb2ecb5ea6fc0c3d
Aurélien Devaux <37188045+aureldvx@users.noreply.github.com>
2024-04-14 15:44:24 +0200

    initial commit

---
+++ src/EcoheadExtraValidatorBundle.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ecohead\ExtraValidatorBundle;
+
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+
+class EcoheadExtraValidatorBundle extends AbstractBundle
+{
+}
